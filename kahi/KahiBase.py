ec5c53d751567644dab73f2b59de8b009d1dea2d
Omar Zapata <Omar.Zapata@cern.ch>
2023-06-14 16:19:27 -0500

    added example run raising an exception

--- kahi/KahiBase.py
+++ kahi/KahiBase.py
@@ -2,3 +2,10 @@
 class KahiBase:
     pass
     # TODO: Implement KahiBase
+
+    def run(self):
+        """
+        entry point for the execution of the plugin, this method must be implemented
+        """
+        raise NotImplementedError(
+            self.__class__.__name__ + '.run() not implemented')
